f7592ed3bcadbeb80741388fc444dcf4f630294f
Your Name <you@example.com>
2024-04-20 16:24:59 -0500

    Update username and password parameters

--- db_connection.php
+++ db_connection.php
@@ -1,14 +1,13 @@
 <?php
 $servername = "fdb1030.awardspace.net";
 $username = "3684404_chatapp";
-$password = "6@[2SH:40#iwSl:2";
+$password = "bh^]s7Vy2[,j^7D.";
 $database = "3684404_chatapp";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 
+
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-
 ?>
