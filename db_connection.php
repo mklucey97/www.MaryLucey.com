e7fcd4212fa3b822663b7daafd4e4982654b2b46
mklucey97 <138608878+mklucey97@users.noreply.github.com>
2024-12-08 21:18:55 -0600

    Add error handling for database connection failure

--- db_connection.php
+++ db_connection.php
@@ -4,5 +4,10 @@ $username = "4413656_chatapp";
 $password = “ABCDEF123456#”;
 $conn = mysqli_connect($servername, $username, $password, $database);
 
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
 
 ?>
