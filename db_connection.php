8dd0d6c905c544c40ba939a38291db5dea67929e
mklucey97 <138608878+mklucey97@users.noreply.github.com>
2024-12-08 19:30:30 -0600

    Add opening php tag

--- db_connection.php
+++ db_connection.php
@@ -1,13 +1,6 @@
 <?php
-$servername = "fdb1030.awardspace.net";
-$username = "3684404_chatapp";
-$password = "bh^]s7Vy2[,j^7D.";
-$database = "3684404_chatapp";
-
-$conn = mysqli_connect($servername, $username, $password, $database);
 
 
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+
+
 ?>
